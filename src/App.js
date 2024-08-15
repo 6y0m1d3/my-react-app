41a185777bf395847694caa258ed779988a8dae7
Saadiq Hamza <hmsaadiq@gmail.com>
2024-08-15 15:39:09 +0100

    im trying to commit .S

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         <code>Welcome</code> 
+         <code>Welcome by S</code> 
         </p>
         <a
           className="App-link"
